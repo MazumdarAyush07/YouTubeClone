efaeb7452226a2f611d0f8fccc1f1380a9a47b7c
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2023-10-31 15:23:10 +0530

    API and Context Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import { AppContext } from "./context/contextApi";
 
 function App() {
-  return <div className="text-2xl">This area will render elements</div>;
+  return (
+    <AppContext>
+      <div className="text-2xl">This area will render elements</div>
+    </AppContext>
+  );
 }
 
 export default App;
---
+++ src/utils/api.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": "6611ac4bf9mshafefee1fcd4f0ddp1cc4cbjsne424e32e537b",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
