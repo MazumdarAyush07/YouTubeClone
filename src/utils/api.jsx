0624a79be1a1504170cc8f199ced8f3064c9f507
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2023-11-02 18:51:58 +0530

    added key

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -3,12 +3,9 @@ import axios from "axios";
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-  params: {
-    hl: "en",
-    gl: "US",
-  },
+  params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key": "6611ac4bf9mshafefee1fcd4f0ddp1cc4cbjsne424e32e537b",
+    "X-RapidAPI-Key": import.meta.env.VITE_APP_YOUTUBE_API_KEY,
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
